b9c0e6d534d0048d2e3ec9490622a5b15c76ad89
wolfpack <wolfpack@vulk.co>
2018-01-22 16:45:02 -0500

    Refactored trigger client to move Ci Service code into lib
     * module/class moved out to lib
     * client in bin simplified to use lib

---
+++ lib/crosscloudci/ciservice_client/build.rb
@@ -0,0 +1,21 @@
+# module CrossCloudCI
+#   module CiService
+#     class Build
+#       attr_accessor :build_id, :project_name, :project_id, :project_ref
+#
+#       def initialize(project_id, build_id, project_name, project_ref)
+#         @project_id, @build_id, @project_name, @project_ref = project_id, build_id, project_name, project_ref
+#       end
+#
+#       def status
+#         build_status(@project_id, @build_id)
+#       end
+#
+#       private
+#
+#       # def build_status
+#       # end
+#     end
+#   end
+# end
+ 
