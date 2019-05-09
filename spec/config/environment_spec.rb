3099ac5b23baef96f7f3966ca5aa5f7fba9fe20d
wolfpack <wolfpack@vulk.co>
2019-05-09 01:55:53 -0400

    vulk/cncf_ci/issues/143 trigger client now tests for arm in yml

--- spec/config/environment_spec.rb
+++ spec/config/environment_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe CrossCloudCi::Common do
       config = CrossCloudCi::Common.init_config
       expect(config[:projects]["kubernetes"]["head_ref"]).to eq "master"
    end
+
+   it "should have an arch array with arm" do
+      config = CrossCloudCi::Common.init_config
+      expect(config[:projects]["kubernetes"]["arch"]).to eq ["amd64", "arm64"] 
+   end
  end
 end 
