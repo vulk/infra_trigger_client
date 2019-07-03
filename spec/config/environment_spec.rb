fb2d836e218a034011ccbd9a9762dc2d2b98c459
wolfpack <wolfpack@vulk.co>
2019-07-03 08:59:52 -0400

    logo test

--- spec/config/environment_spec.rb
+++ spec/config/environment_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe CrossCloudCi::Common do
       expect(config[:projects]["kubernetes"]["head_ref"]).to eq "master"
    end
 
+   it "should read the logo from the crosscloud.yml if present in both crosscloud.yml and cncfci.yml" do
+      config = CrossCloudCi::Common.init_config
+      expect(config[:projects]["linkerd"]["logo_url"]).to eq "https://raw.githubusercontent.com/cncf/artwork/53f1179f7950f27e330fce4e983304de59811b52/linkerd/icon/color/linkerd-icon-color.png"
+      expect(config[:projects]["prometheus"]["logo_url"]).to eq "https://raw.githubusercontent.com/cncf/artwork/1d4e7cf3b60af40e008b2e2413f7a2d1ff784b52/prometheus/icon/color/prometheus-icon-color.png"
+   end
+
    it "should have an arch array with arm" do
       config = CrossCloudCi::Common.init_config
       expect(config[:projects]["kubernetes"]["arch"]).to eq ["amd64", "arm64"] 
