3e406b9e458f1b9072d51bcf18b92848379b2ce8
denverwilliams <denver@debian.nz>
2019-10-16 13:27:56 -0400

    Fix syntax

--- scripts/sync-gitlab-kubernetes.sh
+++ scripts/sync-gitlab-kubernetes.sh
@@ -2,11 +2,11 @@
 
 git clone git@${1}:kubernetes/kubernetes.git /tmp/k8s
 cd /tmp/k8s
-if [[ $? -ne 0]]; then
+if [[ $? -ne 0 ]]; then
     echo 'Git clone failed'
     exit 1
 else
-    continue
+    echo 'Clone successful'
 fi
 git remote add github https://github.com/kubernetes/kubernetes.git
 git fetch github -a
