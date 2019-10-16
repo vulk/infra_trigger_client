c3ca4fd1072f8b7ce27968a972a7c182d07a1bde
denverwilliams <denver@debian.nz>
2019-10-16 12:11:43 -0400

    Fetch all new branches and tags vulk/cncf_ci#265

--- scripts/sync-gitlab-kubernetes.sh
+++ scripts/sync-gitlab-kubernetes.sh
@@ -3,6 +3,7 @@
 git clone git@${1}:kubernetes/kubernetes.git /tmp/k8s
 pushd /tmp/k8s
 git remote add github https://github.com/kubernetes/kubernetes.git
+git fetch github -a
 git pull github master
 K8S_NIGHTLY=$(curl https://storage.googleapis.com/kubernetes-release-dev/ci-cross/latest.txt)
 git reset ${K8S_NIGHTLY#*+}
